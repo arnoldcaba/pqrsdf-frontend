741e8c86cf872f46e577b3cab1196c17bd0d06b9
Arnold Caballero <arnoldcaba@gmail.com>
2022-02-24 21:00:11 -0500

    first deploy

---
+++ precache-manifest.d7c4cc721aa7badc5c67a01c56c79bd1.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "df477b97a90b0a3e291b",
+    "url": "/css/chunk-0dc457a7.19954049.css"
+  },
+  {
+    "revision": "a616f1a08a4efbddeac9",
+    "url": "/js/chunk-2d0e1be2.d6bd7263.js"
+  },
+  {
+    "revision": "862c779655cef58651f6",
+    "url": "/js/chunk-2d20fa8a.abb82cf1.js"
+  },
+  {
+    "revision": "c0f2b75f6b7f5d080426",
+    "url": "/js/chunk-5efb7bfc.4c45c0ef.js"
+  },
+  {
+    "revision": "cafac29d61a2d52e7b99",
+    "url": "/js/chunk-78a1fb8c.ee021b85.js"
+  },
+  {
+    "revision": "20bab138421ef9b7fdec",
+    "url": "/css/chunk-vendors.517ff1ab.css"
+  },
+  {
+    "revision": "20bab138421ef9b7fdec",
+    "url": "/js/chunk-vendors.7f888750.js"
+  },
+  {
+    "revision": "128814e08d97206dcdc084bca7aeaf8c",
+    "url": "/img/logo.128814e0.png"
+  },
+  {
+    "revision": "31dac32fbfe942c0daa8e8e5e372db25",
+    "url": "/img/ImgM_blanco.31dac32f.png"
+  },
+  {
+    "revision": "b1b8e789ba67124fc20f95a25f121d03",
+    "url": "/img/ImgH_blanco.b1b8e789.png"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "cc2b18481087ca70b016",
+    "url": "/js/app.1508372d.js"
+  },
+  {
+    "revision": "df477b97a90b0a3e291b",
+    "url": "/js/chunk-0dc457a7.99a2c094.js"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "165aadb4effbe330723cdf9b2651133c",
+    "url": "/img/ITIM.165aadb4.png"
+  },
+  {
+    "revision": "1a478560e45a20eae2a6411d68047543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7af09d8f513a98a93aa4b886ae68016",
+    "url": "/img/ImgM_blanco.png"
+  },
+  {
+    "revision": "11ba8fc2a429438ab35c07cbe583a2ae",
+    "url": "/img/ImgH_blanco.png"
+  },
+  {
+    "revision": "456219e2c16103f9fe330a30f5e7064f",
+    "url": "/img/logo_footer_right.png"
+  },
+  {
+    "revision": "257fd48e8c11302f2263d4ab36ef77b0",
+    "url": "/img/logo_footer_left.png"
+  },
+  {
+    "revision": "8b18be7664d82908bf5f2bd7f47c0a91",
+    "url": "/img/logo_head_left.png"
+  },
+  {
+    "revision": "758ed7bcd8dcb95f8b6e4004b21143b7",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "7961b53f5a316cfbeb6dc13f3c24c634",
+    "url": "/img/logo_head_right.png"
+  },
+  {
+    "revision": "cc2b18481087ca70b016",
+    "url": "/css/app.35e9512f.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.d7c4cc721aa7badc5c67a01c56c79bd1.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "PQRSDF"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
